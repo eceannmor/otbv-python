33edcf40230b4ebbef9e4a0d7e0103b8b766a007
Ekre Ceannmor <126588178+eceannmor@users.noreply.github.com>
2025-10-09 23:28:35 +0200

    stuff

--- src/octvencode.py
+++ src/octvencode.py
@@ -1,15 +1,42 @@
+"""!
+@package octvencode
+@file octvencode.py
+"""
+
 import numpy as np
 import struct
 
-LATEST_FORMAT_VERSION = "0.0.1"
+__ALLOWED_TYPES = (int, float, np.number, bool, np.bool)
+__LATEST_FORMAT_VERSION = "0.0.1"
 
-def reshape_volume_to_cubic(volume: np.ndarray) -> np.ndarray:
+def __convert_or_throw(o) -> np.ndarray:
+    """!
+    @brief Attempts to convert \p o into a non-empty NumPy array. \n For detailed data conversion rules, check docs 
+    @throws TypeError if the conversion is not possible
+    @return \p o represented as a numpy array, if possible
     """
-    Attempts to resize the given volume into 3 equal dimensions
-    Throws if not possible
+    if isinstance(o, np.ndarray) and o.dtype in __ALLOWED_TYPES:
+        return o
+    if isinstance(o, __ALLOWED_TYPES):
+        return np.array(o)
+    try:
+        arr = np.asarray(o)
+        if arr.dtype == object and all([isinstance(x, __ALLOWED_TYPES) for x in arr.flat]):
+            return arr
+        else:
+            raise TypeError(f"Provided data is of an unsupported type (received {arr.dtype}, expected {__ALLOWED_TYPES})")
+    except:
+        raise TypeError("Provided object is not a valid NumPy array and cannot be converted to a NumPy array")
+        
+        
+
+def _reshape_volume_to_cubic(volume: np.ndarray) -> np.ndarray:
+    """!
+    @brief Reshapes a NumPy array into a cube. \n Returns a copy of the data, \p volume remains unchanged
+    @throws ValueError if \p volume cannot be reshaped into a cubic array
+    @returns A cubic NumPy array with the same data as volume
     """
-    if not isinstance(volume, np.ndarray):
-        raise TypeError("Provided volume is not a valid Numpy ndarray")
+    volume = __convert_or_throw(volume)
     
     size = volume.size
     new_edge_len = np.cbrt(size)
@@ -19,36 +46,36 @@ def reshape_volume_to_cubic(volume: np.ndarray) -> np.ndarray:
     return np.reshape(volume, (new_edge_len, new_edge_len, new_edge_len))   
     
 
-def is_volume_slice_homogeneous(vol_slice: np.ndarray) -> bool:
+def _is_volume_homogeneous(subvolume: np.ndarray) -> bool:
+    """!
+    @returns True if all elements in \p subvolume are the same
+    @returns False otherwise, or if \p subvolume is empty
     """
-    Checks if the volume slice's values are homogeneous, i.e. if there is a need to split.
-    """
-    if not isinstance(vol_slice, np.ndarray):
-        raise TypeError("Provided volume is not a valid Numpy ndarray")
-    if vol_slice.size < 2:
+    subvolume = __convert_or_throw(subvolume)
+    if subvolume.size < 2:
         return True
-    return np.all(vol_slice == vol_slice.flat[0])
+    return np.all(subvolume == subvolume.flat[0])
 
 
-def encode_slice_recursive(volume: np.ndarray, x_start: int, x_end: int,
+def __encode_subvolume_recursive(volume: np.ndarray, x_start: int, x_end: int,
                            y_start: int, y_end: int, z_start: int,
                            z_end: int) -> str:
-    """
-    Checks if the volume needs to be sliced, if not, returns immediately with the value of the slice
-    Else slices the volume and calls itself for all slices
+    """!
+    @brief Helper function. Encodes a subvolume. If the subvolume is homogeneous, returns a leaf bit and a value bit, else reterns a split bit as well as the concatenated encodings of all subvolumes of its own. \n The concatenation order is defined by the format.
+    @return The encoding of the subvolume
     """
 
-    vol_slice = volume[x_start:x_end, y_start:y_end, z_start:z_end]
-    assert vol_slice.size > 0, f"Encountered a slice with a size of 0 when encoding the volume. This should not happen. Parameters: {x_start} {x_end} {y_start} {y_end} {z_start} {z_end}. Please open a new issue and attach the data you are trying to encode. https://github.com/eceannmor/octvencode-python/issues"
-    if is_volume_slice_homogeneous(vol_slice):
+    subvolume = volume[x_start:x_end, y_start:y_end, z_start:z_end]
+    assert subvolume.size > 0, f"Encountered a subvolume with a size of 0 when encoding the volume. This should never happen. Parameters: {x_start} {x_end} {y_start} {y_end} {z_start} {z_end}. Please open a new issue and attach the data you are trying to encode. https://github.com/eceannmor/octvencode-python/issues"
+    if is_volume_homogeneous(subvolume):
         # leaf
-        return f"0{int(vol_slice.flat[0])}"
+        return f"0{int(subvolume.flat[0])}"
 
     x_split = [x_start, (x_start + x_end) // 2, x_end]
     y_split = [y_start, (y_start + y_end) // 2, y_end]
     z_split = [z_start, (z_start + z_end) // 2, z_end]
 
-    slices = []
+    subvolumes = []
 
     # order:
     # (0, 0, 0)
@@ -62,20 +89,19 @@ def encode_slice_recursive(volume: np.ndarray, x_start: int, x_end: int,
     for z in (0, 1):
         for y in (0, 1):
             for x in (0, 1):
-                slices.append(
-                    encode_slice_recursive(volume, x_split[x], x_split[x + 1],
+                subvolumes.append(
+                    encode_subvolume_recursive(volume, x_split[x], x_split[x + 1],
                                            y_split[y], y_split[y + 1],
                                            z_split[z], z_split[z + 1]))
     # slice
-    return f"1{"".join(slices)}"
+    return f"1{"".join(subvolumes)}"
 
 
-def encode(volume: np.ndarray, format_version = LATEST_FORMAT_VERSION) -> str:
+def encode(volume: np.ndarray, format_version = __LATEST_FORMAT_VERSION) -> str:
     """
     Encode a bit volume as an octree and convert it to a string.
     """
-    if not isinstance(volume, np.ndarray):
-        raise TypeError("Provided volume is not a valid Numpy ndarray")
+    volume = __convert_or_throw(volume)
 
     shape = volume.shape
     if len(shape) != 3:
@@ -95,12 +121,12 @@ def encode(volume: np.ndarray, format_version = LATEST_FORMAT_VERSION) -> str:
             f"Passed volume with invalid resolution. Expected a power of 2, received {resolution}."
         )
 
-    string = encode_slice_recursive(volume, 0, resolution, 0, resolution, 0,
+    string = encode_subvolume_recursive(volume, 0, resolution, 0, resolution, 0,
                                     resolution)
     return f"{resolution} {string}"
 
 
-def decode_slice_recursive(tokens, idx, volume, x_start, x_end, y_start, y_end,
+def __decode_subvolume_recursive(tokens, idx, volume, x_start, x_end, y_start, y_end,
                            z_start, z_end):
     if idx >= len(tokens):
         raise ValueError("Unexpected end of encoding")
@@ -122,7 +148,7 @@ def decode_slice_recursive(tokens, idx, volume, x_start, x_end, y_start, y_end,
         for z in (0, 1):
             for y in (0, 1):
                 for x in (0, 1):
-                    idx = decode_slice_recursive(tokens, idx, volume,
+                    idx = decode_subvolume_recursive(tokens, idx, volume,
                                                  x_split[x], x_split[x + 1],
                                                  y_split[y], y_split[y + 1],
                                                  z_split[z], z_split[z + 1])
@@ -131,7 +157,7 @@ def decode_slice_recursive(tokens, idx, volume, x_start, x_end, y_start, y_end,
     raise ValueError(f"Invalid token '{token}' at position {idx-1}")
 
 
-def decode(resolution: int, data: str, format_version = LATEST_FORMAT_VERSION) -> np.ndarray:
+def decode(resolution: int, data: str, format_version = __LATEST_FORMAT_VERSION) -> np.ndarray:
     """
     Decode a string representing an octree and save it as a bit volume
     """
@@ -159,12 +185,12 @@ def decode(resolution: int, data: str, format_version = LATEST_FORMAT_VERSION) -
 
     volume = np.zeros((resolution, resolution, resolution), dtype=np.uint8)
 
-    final_idx = decode_slice_recursive(tokens, 0, volume, 0, resolution, 0,
+    final_idx = decode_subvolume_recursive(tokens, 0, volume, 0, resolution, 0,
                                        resolution, 0, resolution)
     return volume
 
 
-def to_file(data: str, format_version = LATEST_FORMAT_VERSION):
+def to_file(data: str, format_version = __LATEST_FORMAT_VERSION):
     size, data = data.split(" ", 1)
     pad_len = len(data) % 8
     if pad_len:
@@ -179,5 +205,5 @@ def to_file(data: str, format_version = LATEST_FORMAT_VERSION):
         f.write(b'\x00')
         f.write(byte_data)
 
-def from_file(filename, format_version = LATEST_FORMAT_VERSION):
+def from_file(filename, format_version = __LATEST_FORMAT_VERSION):
     pass
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -6,49 +6,46 @@ class TestUnit:
 
     def test_homogeneity(self):
         volume1 = np.zeros(10)
-        assert octvencode.is_volume_slice_homogeneous(volume1)
+        assert octvencode._is_volume_homogeneous(volume1)
         
         volume2 = np.zeros(10)
         volume2[3] = 1
-        assert not octvencode.is_volume_slice_homogeneous(volume2)
+        assert not octvencode._is_volume_homogeneous(volume2)
         
         volume3 = np.array([1])
-        assert octvencode.is_volume_slice_homogeneous(volume3)
+        assert octvencode._is_volume_homogeneous(volume3)
         
         volume4 = np.array([])
-        assert octvencode.is_volume_slice_homogeneous(volume4)
+        assert octvencode._is_volume_homogeneous(volume4)
         
         volume5 = "abcd"
         with pytest.raises(TypeError) as err1:
-            octvencode.is_volume_slice_homogeneous(volume5)
-        assert err1.type == TypeError
-        assert err1.value.args[0] == "Provided volume is not a valid Numpy ndarray"
+            octvencode._is_volume_homogeneous(volume5)
+            assert err1.type == TypeError
         
         volume6 = np.zeros(1000)
         volume6.reshape((10,10,10))
-        assert octvencode.is_volume_slice_homogeneous(volume6)
+        assert octvencode._is_volume_homogeneous(volume6)
         
         volume7 = np.zeros(1000)
         volume7[17] = 1
         volume7.reshape((10,10,10))
-        assert not octvencode.is_volume_slice_homogeneous(volume7)
+        assert not octvencode._is_volume_homogeneous(volume7)
         
     
     def test_reshaping(self):
         volume1 = np.zeros(1000)
-        volume1_reshaped = octvencode.reshape_volume_to_cubic(volume1)
+        volume1_reshaped = octvencode._reshape_volume_to_cubic(volume1)
         assert volume1_reshaped.shape == (10,10,10)
         assert volume1_reshaped.size == volume1.size
         
         volume2 = np.zeros(999)
         with pytest.raises(ValueError) as err1:
-            octvencode.reshape_volume_to_cubic(volume2)
-        assert err1.type == ValueError
-        assert err1.value.args[0] == f"Could not reshape a volume with dimensions {volume2.shape} into a cubic 3d volume"
+            octvencode._reshape_volume_to_cubic(volume2)
+            assert err1.type == ValueError
         
         volume3 = "abcd"
         with pytest.raises(TypeError) as err2:
-            octvencode.reshape_volume_to_cubic(volume3)
-        assert err2.type == TypeError
-        assert err2.value.args[0] == "Provided volume is not a valid Numpy ndarray"
+            octvencode._reshape_volume_to_cubic(volume3)
+            assert err2.type == TypeError
         
